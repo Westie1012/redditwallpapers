632999c2bb56106a07b3e0071f0d5ac2afecd879
Alex West <westie1012@gmail.com>
2016-09-20 20:51:50 +0100

    Progress

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import ctypes
 import os
 import random, urllib.request, json
+global drive, folder, image
 
 drive = "C:\\"
 folder = "images"
+image = "test.jpg"
 f = open("url.txt")
 urls = []
-for line in f:
-    urls.append(line.strip())
-f.close()
+# "https://www.reddit.com/r/MinimalWallpaper/.json"
+subreddit = "http://pastebin.com/raw/DLNYEhNw"
 
-image = "test.jpg"
-image_path = os.path.join(drive, folder, image)
-SPI_SETDESKWALLPAPER = 20 
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, image_path, 3)
-subreddit = "https://www.reddit.com/r/
+
+def setImage(image):
+    image_path = os.path.join(drive, folder, image)
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoA(
+        SPI_SETDESKWALLPAPER, 0, image_path,)
+
+
+def validate(url):
+    pass
+
+
+url = urllib.request.urlopen(subreddit)
+content = url.read()
+data = json.loads(content.decode("utf-8"))
+for i in data["data"]["children"]:
+    urls.append(i["data"]["url"] + "\n")
+
+for item in urls:
+    validate(item)
